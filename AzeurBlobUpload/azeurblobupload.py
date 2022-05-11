e2b18feb21777312c52ce8bf103f76396d9d2462
Tariq Carrim <u20581018@tuks.co.za>
2022-05-12 00:30:17 +0200

    Azure blobs database

---
+++ AzeurBlobUpload/azeurblobupload.py
@@ -0,0 +1,10 @@
+from azure.storage.blob import BlobClient
+
+blobname ="unaizah.png"
+Connection_String = "DefaultEndpointsProtocol=https;AccountName=choppercharlie;AccountKey=Bcrvc/ix8TmB/hoEE2fmp44iHAqEWeiZ1fr7Fml9Z0+Q7RI8NvX2kbqzeufPKHRY54hk+wFgE/+a+AStzl2qTw==;EndpointSuffix=core.windows.net"
+Containers_Name = "media"
+blob = BlobClient.from_connection_string(conn_str= Connection_String, container_name= Containers_Name, blob_name= blobname)
+
+
+with open("/home/tariqcarrim/Desktop/bb.png", "rb") as data:
+    blob.upload_blob(data)
