9c47ba2c5b577b84e2ec916c2e8dc43f4b4012e6
Yaasir Barmania <93663409+Yaasir25@users.noreply.github.com>
2022-05-12 23:27:49 +0200

    Update App.js

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 function App() {
   return (
     <div>
-      <video  controls >
-        <source src="https://choppercharlie.blob.core.windows.net/media/Cars.mp4" type="video/mp4"/>
-      </video>
           <br></br>
         <img src="https://choppercharlie.blob.core.windows.net/media/carsanalyzed.png" />
     </div>
