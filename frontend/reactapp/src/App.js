49a715bb24c2608bc1c9d9aa2d8152de59f02881
Yaasir Barmania <93663409+Yaasir25@users.noreply.github.com>
2022-05-12 23:24:12 +0200

    Update App.js

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
         <source src="https://choppercharlie.blob.core.windows.net/media/Cars.mp4" type="video/mp4"/>
       </video>
           <br></br>
-        <img src="https://choppercharlie.blob.core.windows.net/media/alotofcars.jpg" />
+        <img src="https://choppercharlie.blob.core.windows.net/media/carsanalyzed.png" />
     </div>
   );
 }
