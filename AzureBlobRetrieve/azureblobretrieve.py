908c6a660b95c5e6edac10794ee77f21c98e2033
Tariq Carrim <u20581018@tuks.co.za>
2022-05-13 02:24:58 +0200

    fixes

--- AzureBlobRetrieve/azureblobretrieve.py
+++ AzureBlobRetrieve/azureblobretrieve.py
@@ -1,5 +1,5 @@
 from azure.storage.blob import BlobClient
-blobname = "alotofcars.jpg"
+blobname = "unaizah.jpg"
 Connection_String = "DefaultEndpointsProtocol=https;AccountName=choppercharlie;AccountKey=Bcrvc/ix8TmB/hoEE2fmp44iHAqEWeiZ1fr7Fml9Z0+Q7RI8NvX2kbqzeufPKHRY54hk+wFgE/+a+AStzl2qTw==;EndpointSuffix=core.windows.net"
 Containers_Name = "media"
 blob = BlobClient.from_connection_string(conn_str=Connection_String, container_name=Containers_Name, blob_name=blobname)
--- AzureBlobUpload/azureblobupload.py
+++ AzureBlobUpload/azureblobupload.py
@@ -1,9 +1,9 @@
 from azure.storage.blob import BlobClient
 
-blobname ="carss.jpg"
+blobname ="unaizah.jpg"
 Connection_String = "DefaultEndpointsProtocol=https;AccountName=choppercharlie;AccountKey=Bcrvc/ix8TmB/hoEE2fmp44iHAqEWeiZ1fr7Fml9Z0+Q7RI8NvX2kbqzeufPKHRY54hk+wFgE/+a+AStzl2qTw==;EndpointSuffix=core.windows.net"
 Containers_Name = "media"
 blob = BlobClient.from_connection_string(conn_str= Connection_String, container_name= Containers_Name, blob_name= blobname)
 
-with open("carss.jpg", "rb") as data:
+with open("unaizah.jpg", "rb") as data:
     blob.upload_blob(data)
