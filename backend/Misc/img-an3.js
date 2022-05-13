171da270e19646f972fc41a30465a22e5a44720b
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-05-13 10:08:29 +0200

    js imports

---
+++ backend/Misc/img-an3.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const async = require('async');
+const fs = require('fs');
+const https = require('https');
+const path = require("path");
+const createReadStream = require('fs').createReadStream
+const sleep = require('util').promisify(setTimeout);
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
