b5f449b4043e7d26be29358cc182f5670fb37a71
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-05-13 10:19:53 +0200

    java analyzer

---
+++ backend/Misc/img-an4.java
@@ -0,0 +1,10 @@
+import com.microsoft.azure.cognitiveservices.vision.computervision.*;
+import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionImpl;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
+
+import java.io.*;
+import java.nio.file.Files;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
