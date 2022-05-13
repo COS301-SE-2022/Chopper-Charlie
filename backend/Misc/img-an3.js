69f9dd953b7882e4f23fbbe7453e30209e5a8bf9
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-05-13 10:09:37 +0200

    js authentication

--- backend/Misc/img-an3.js
+++ backend/Misc/img-an3.js
@@ -8,3 +8,9 @@ const createReadStream = require('fs').createReadStream
 const sleep = require('util').promisify(setTimeout);
 const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
 const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+
+const key = 'PASTE_YOUR_COMPUTER_VISION_SUBSCRIPTION_KEY_HERE';
+const endpoint = 'PASTE_YOUR_COMPUTER_VISION_ENDPOINT_HERE';
+
+const computerVisionClient = new ComputerVisionClient(
+  new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
