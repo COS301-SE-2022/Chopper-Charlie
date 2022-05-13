4154621b7f52998089795fbeb1cc2678e9d0ca53
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-05-13 09:55:08 +0200

    cs imports

---
+++ backend/Misc/img-an2.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
