d2ca1b88d528cc5dd5fc3182baece9d4ecbecffb
Tariq Carrim <u20581018@tuks.co.za>
2022-06-06 16:56:06 +0200

    Created Azure functions

--- AzureBlobUpload/azureblobupload.py
+++ AzureBlobUpload/azureblobupload.py
@@ -1,9 +1,16 @@
 from azure.storage.blob import BlobClient
 
-blobname ="unaizah.png"
 Connection_String = "DefaultEndpointsProtocol=https;AccountName=choppercharlie;AccountKey=Bcrvc/ix8TmB/hoEE2fmp44iHAqEWeiZ1fr7Fml9Z0+Q7RI8NvX2kbqzeufPKHRY54hk+wFgE/+a+AStzl2qTw==;EndpointSuffix=core.windows.net"
-Containers_Name = "media"
-blob = BlobClient.from_connection_string(conn_str= Connection_String, container_name= Containers_Name, blob_name= blobname)
 
-with open("unaizah.png", "rb") as data:
-    blob.upload_blob(data)
+def blob_upload(Blob_Name, Containers_Name):
+    try:
+        blob = BlobClient.from_connection_string(conn_str= Connection_String, container_name= Containers_Name, blob_name= Blob_Name)
+
+        with open(Blob_Name, "rb") as data:
+            blob.upload_blob(data)
+    except:
+        print('Error! Try Renaming The Image or Video')
+        
+
+#Input Image Name And Container Name (ALSO MAKE SURE IMAGE IS IN CHOPPER CHARLIE DIRECTORY!)        
+blob_upload("Test88.jpg", "media")
