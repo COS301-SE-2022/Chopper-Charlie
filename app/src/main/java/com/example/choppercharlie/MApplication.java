28ea58855459b774f9807238558dd7e905eb91a4
Unaizah Mahomed <unaizahmahomed@gmail.com>
2022-06-09 23:09:34 +0200

    Fix

--- app/src/main/java/com/example/choppercharlie/MApplication.java
+++ app/src/main/java/com/example/choppercharlie/MApplication.java
@@ -8,4 +8,5 @@ public class MApplication extends Application{
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
     }
+    //this helps the drone connect but is still not working 100%
 }
