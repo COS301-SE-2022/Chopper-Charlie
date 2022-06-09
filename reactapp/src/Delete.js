fa9723281147229a325bf7bc7c82ffa05b160433
Tariq Carrim <u20581018@tuks.co.za>
2022-06-09 20:18:08 +0200

    fix

--- reactapp/src/Delete.js
+++ reactapp/src/Delete.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {useAuthValue} from './AuthContext'
 import './profile.css'
 import DeleteIcon from '@mui/icons-material/Delete';
-import { color } from "@mui/system";
 
 //i hope it worked
 
