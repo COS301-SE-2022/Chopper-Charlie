f0050f31c4f9f27bae2edd77da1c038f536a3d23
MumtazMalik01 <93917949+MumtazMalik01@users.noreply.github.com>
2022-06-09 20:42:25 +0200

    Update index.js

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -15,6 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
 
 reportWebVitals();
