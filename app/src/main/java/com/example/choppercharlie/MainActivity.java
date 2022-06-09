9fb1f6ab897b609e456f837f5a683dee5a879990
Unaizah Mahomed <unaizahmahomed@gmail.com>
2022-06-09 23:19:50 +0200

    Fix

--- app/src/main/java/com/example/choppercharlie/MainActivity.java
+++ app/src/main/java/com/example/choppercharlie/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private List<String> missingPermission = new ArrayList<>();
     private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
     private static final int REQUEST_PERMISSION_CODE = 12345;
-    //need help with this must contact industry mentor
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
