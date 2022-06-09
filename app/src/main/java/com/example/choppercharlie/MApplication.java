fb159eff0fc929fd6f05bd75ed3044137350a30d
Unaizah Mahomed <unaizahmahomed@gmail.com>
2022-06-09 23:13:39 +0200

    Fix

--- app/src/main/java/com/example/choppercharlie/MApplication.java
+++ app/src/main/java/com/example/choppercharlie/MApplication.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import com.secneo.sdk.Helper;
 public class MApplication extends Application{
     @Override
+    //this helps the drone connect but is still not working 100%
+    //need to connect to drone
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
