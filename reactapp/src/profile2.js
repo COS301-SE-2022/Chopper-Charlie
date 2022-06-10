ec8617b967667d9b004c05d4c963e3af097355d2
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-06-10 02:06:19 +0200

    List Transfer

--- reactapp/src/profile2.js
+++ reactapp/src/profile2.js
@@ -39,39 +39,7 @@ const drawerWidth = 240;
 export default function profile2() {
   return (
 
-    <Paper sx={{ width: 320, maxWidth: '100%' }}>
-    <MenuList>
-      <MenuItem>
-        <ListItemIcon>
-          <ContentCut fontSize="small" />
-        </ListItemIcon>
-        <ListItemText>Cut</ListItemText>
-        <Typography variant="body2" color="text.secondary">
-          ⌘X
-        </Typography>
-      </MenuItem>
-      <MenuItem>
-        <ListItemIcon>
-          <ContentCopy fontSize="small" />
-        </ListItemIcon>
-        <ListItemText>Copy</ListItemText>
-        <Typography variant="body2" color="text.secondary">
-          ⌘C
-        </Typography>
-      </MenuItem>
-      <MenuItem>
-        <ListItemIcon>
-          <ContentPaste fontSize="small" />
-        </ListItemIcon>
-        <ListItemText>Paste</ListItemText>
-        <Typography variant="body2" color="text.secondary">
-          ⌘V
-        </Typography>
-      </MenuItem>
-      <Divider />
 
-    </MenuList>
-  </Paper>
 
 
     <Box sx={{ display: 'flex' }}>
