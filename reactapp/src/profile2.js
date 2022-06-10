1fbd828b490a237e7f967076b6be411c9118352d
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-06-10 02:18:45 +0200

    Mock 4

--- reactapp/src/profile2.js
+++ reactapp/src/profile2.js
@@ -254,5 +254,10 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
     author: '@rollelflex_graphy726',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+    author: '@helloimnik',
   }
 ];
