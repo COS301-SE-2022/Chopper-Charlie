6609c9a30e3988eaf022c20a3e048b461787b028
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-06-10 02:00:32 +0200

    List

--- reactapp/src/profile2.js
+++ reactapp/src/profile2.js
@@ -29,6 +29,47 @@ const drawerWidth = 240;
 
 export default function profile2() {
   return (
+
+    <Paper sx={{ width: 320, maxWidth: '100%' }}>
+    <MenuList>
+      <MenuItem>
+        <ListItemIcon>
+          <ContentCut fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Cut</ListItemText>
+        <Typography variant="body2" color="text.secondary">
+          ⌘X
+        </Typography>
+      </MenuItem>
+      <MenuItem>
+        <ListItemIcon>
+          <ContentCopy fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Copy</ListItemText>
+        <Typography variant="body2" color="text.secondary">
+          ⌘C
+        </Typography>
+      </MenuItem>
+      <MenuItem>
+        <ListItemIcon>
+          <ContentPaste fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Paste</ListItemText>
+        <Typography variant="body2" color="text.secondary">
+          ⌘V
+        </Typography>
+      </MenuItem>
+      <Divider />
+      <MenuItem>
+        <ListItemIcon>
+          <Cloud fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>Web Clipboard</ListItemText>
+      </MenuItem>
+    </MenuList>
+  </Paper>
+
+
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar
