f3d5a3a02a22fcca73b04a828d68ccf43f3a0537
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-06-10 02:24:39 +0200

    Mock 10

--- reactapp/src/profile2.js
+++ reactapp/src/profile2.js
@@ -289,5 +289,28 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
     title: 'Fern',
     author: '@katie_wasserman',
-  }
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+    author: '@southside_customs',
+    cols: 2,
+  },
 ];
