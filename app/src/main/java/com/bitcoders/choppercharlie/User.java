af56c57e944f80755037a532511eb9deaa90495a
Yaasir Barmania <yaasirbarmania@gmail.com>
2022-07-21 00:12:31 +0200

    Added file

---
+++ app/src/main/java/com/bitcoders/choppercharlie/User.java
@@ -0,0 +1,11 @@
+package com.bitcoders.choppercharlie;
+
+public class User {
+    public String email;
+
+    public User(){}
+
+    public  User(String email){
+        this.email=email;
+    }
+}
