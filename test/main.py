db19210c0be9f551c9c06a2af50984046fc1dab3
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-07-29 01:52:24 +0200

    setup test

--- test/main.py
+++ test/main.py
@@ -3,3 +3,6 @@ from selenium import webdriver
 import page
 
 class ChopperTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome("F:\Coding\Selenium\chromedriver.exe")
+        self.driver.get("http://localhost:3000/login")
