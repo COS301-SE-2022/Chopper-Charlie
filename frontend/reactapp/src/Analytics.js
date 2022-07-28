8022c2b200ca2b237afa20b663343d35a72ac27a
MumtazMalik01 <93917949+MumtazMalik01@users.noreply.github.com>
2022-07-29 01:07:38 +0200

    update

--- frontend/reactapp/src/Analytics.js
+++ frontend/reactapp/src/Analytics.js
@@ -66,6 +66,12 @@ function Profile() {
         <img src={require('./play.png')} width="30%" height="40%" alt="Logo"/>
         <h4>Watch Live Stream here</h4>
         </div> */}
+
+    {/* <div className='Lv'>
+        <img src={require('./play.png')} width="30%" height="40%" alt="Logo"/>
+        <h4>Watch Live Stream here</h4>
+        </div> */}
+
 {/* can delete using id number */}
 
          </div>
