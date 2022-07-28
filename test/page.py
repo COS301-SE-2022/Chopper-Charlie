09214cc0ae64ebd8e6c44becf5b3299ce30eb4a6
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-07-29 01:41:39 +0200

    del

--- test/page.py
+++ test/page.py
@@ -44,7 +44,8 @@ class testPage(BasePage):
         self.driver.get("http://localhost:3000/upload")
     
     def delete_page(self):
-
+        self.click_login()
+        self.driver.get("http://localhost:3000/delete")
     
     def download_page(self):
 
