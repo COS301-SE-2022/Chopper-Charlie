ca1780c120c6405e6c9aa0a857c10e8883690d30
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-07-29 01:52:39 +0200

    upload test

--- test/main.py
+++ test/main.py
@@ -6,3 +6,7 @@ class ChopperTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome("F:\Coding\Selenium\chromedriver.exe")
         self.driver.get("http://localhost:3000/login")
+
+    def test_Upload(self):
+        updatePage = page.testPage(self.driver)
+        assert updatePage.upload_file()
