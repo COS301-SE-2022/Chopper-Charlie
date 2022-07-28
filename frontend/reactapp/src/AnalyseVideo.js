3553d2a182b11eee62c22ba809604c040e8000f8
MumtazMalik01 <93917949+MumtazMalik01@users.noreply.github.com>
2022-07-28 22:08:07 +0200

    Added div

--- frontend/reactapp/src/AnalyseVideo.js
+++ frontend/reactapp/src/AnalyseVideo.js
@@ -189,6 +189,8 @@ function AnalyseVideo() {
           
           <p id="demo"></p> 
           </div> <button id='delete' onClick={del}><DeleteIcon id='icon'/></button>
+		  <button id='delete' onClick={del}><DeleteIcon id='icon'/></button>
+		  
 
 
 
