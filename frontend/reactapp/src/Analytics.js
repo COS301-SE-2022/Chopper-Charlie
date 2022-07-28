c07bf6d9bbaf38a13dbabeaae8e7d21862a86141
MumtazMalik01 <93917949+MumtazMalik01@users.noreply.github.com>
2022-07-29 01:14:00 +0200

    update ui

--- frontend/reactapp/src/Analytics.js
+++ frontend/reactapp/src/Analytics.js
@@ -63,14 +63,6 @@ function Profile() {
         <h4>Watch Live Stream here</h4>
         </div> */
 
-    
-   
-
-    {/* <div className='Lv'>
-        <img src={require('./play.png')} width="30%" height="40%" alt="Logo"/>
-        <h4>Watch Live Stream here</h4>
-        </div> */}
-
     {/* <div className='Lv'>
         <img src={require('./play.png')} width="30%" height="40%" alt="Logo"/>
         <h4>Watch Live Stream here</h4>
