f8ed6494a1ebd453daf4edb99c552a3aaa4acd06
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-07-29 01:36:12 +0200

    Login test

--- test/page.py
+++ test/page.py
@@ -16,3 +16,5 @@ class testPage(BasePage):
         username = self.driver.find_element("id", "username")
         username.send_keys("u20486783@tuks.co.za")
         password = self.driver.find_element("id", "pass")
+        password.send_keys("12345678")
+        lgnbtn = self.driver.find_element("id", "login")
