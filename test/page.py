f02f165f95f624fc9dd14f79638f1deeeb8c4ce5
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-07-29 01:34:06 +0200

    Base Object Page

--- test/page.py
+++ test/page.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import os
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
