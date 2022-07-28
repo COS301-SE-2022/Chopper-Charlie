1e8b59f7f30ececa52f7b4cc66e61c8f58ab2d0f
MumtazMalik01 <93917949+MumtazMalik01@users.noreply.github.com>
2022-07-29 01:03:55 +0200

    refining

--- frontend/reactapp/src/Analytics.js
+++ frontend/reactapp/src/Analytics.js
@@ -30,14 +30,7 @@ function Profile() {
 //         document.getElementById(pNo).style.visibility="hidden";
 //     }    
     
-    
-    
-    
-    {/* <div className='Lv'>
-        <img src={require('./play.png')} width="30%" height="40%" alt="Logo"/>
-        <h4>Watch Live Stream here</h4>
-        </div> */}
-     
+
 
 }
 
