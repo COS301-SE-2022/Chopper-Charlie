e9436147ef387ac3f0e42e61f282ba743e609130
Tariq Carrim <u20581018@tuks.co.za>
2022-08-15 09:25:35 +0200

    Fixes

--- backend/AzureBlobRetrieve/azureblobretrieve.py
+++ backend/AzureBlobRetrieve/azureblobretrieve.py
@@ -1,4 +1,7 @@
 from azure.storage.blob import BlobClient
+from tkinter import messagebox
+import tkinter
+from tkinter import filedialog
 
 Connection_String = "DefaultEndpointsProtocol=https;AccountName=choppercharlie;AccountKey=Bcrvc/ix8TmB/hoEE2fmp44iHAqEWeiZ1fr7Fml9Z0+Q7RI8NvX2kbqzeufPKHRY54hk+wFgE/+a+AStzl2qTw==;EndpointSuffix=core.windows.net"
 
@@ -13,8 +16,11 @@ def blob_retrieve(Blob_Name, Containers_Name):
             stream = blob.download_blob()
             data = stream.readall()
             my_blob.write(data)
+            messagebox.showinfo("Success","Successfully Downloaded media")  #added now
+
     except:
         print('Error!')
+    
 #
 #Input Image Name And Container Name
 #blob_retrieve("Test88.jpg","media")
