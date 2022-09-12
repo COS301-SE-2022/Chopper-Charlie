ecb6e998ed1f9ff77327eae809c75b5238b98908
Yaasir Barmania <yaasirbarmania@gmail.com>
2022-09-12 14:33:52 +0200

    testing

---
+++ testing/main.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+import page
+
+class ChopperTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome("F:\Coding\Selenium\chromedriver.exe")
+        self.driver.get("http://localhost:3000/login")
+
+    def test_Upload(self):
+        updatePage = page.testPage(self.driver)
+        assert updatePage.upload_file()
+
+    def test_Analysis(self):
+        print('Testing Pipeline Creation..\n')
+        analysisPage = page.testPage(self.driver)
+        analysisPage.analyse_File()
+        assert True 
+
+    def test_Download(self):
+        print('Testing Download..\n')
+        downPage = page.testPage(self.driver)
+        assert downPage.download_File()
+
+    def test_Delete(self):
+        print('Testing Delete..\n')
+        fname = "image-testing.png"
+        delete = page.testPage(self.driver)
+        assert delete.delete_file(fname)
+
+    def test_Logout(self):
+        print('Testing Logout..\n')
+        profile = page.testPage(self.driver)
+        assert profile.click_logout()
+
+    def test_Login(self):
+        print('Testing Login..\n')
+        loginPage = page.testPage(self.driver)
+        assert loginPage.is_login()
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
