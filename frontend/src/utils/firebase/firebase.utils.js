ed6cce44132aa6e696d77439e3c72db2d1e747bc
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 13:55:47 +0200

    firebase-auth-listener

--- frontend/src/utils/firebase/firebase.utils.js
+++ frontend/src/utils/firebase/firebase.utils.js
@@ -175,3 +175,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+	onAuthStateChanged(auth, callback);
