7fb1782b4886653e0691a482ece5f527bcb34c6f
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 13:17:49 +0200

    createButton

--- frontend/src/components/button/button.styles.jsx
+++ frontend/src/components/button/button.styles.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import Button from '@mui/material/Button';
 
 export const BaseButton = styled.button`
   min-width: 165px;
@@ -77,3 +79,15 @@ export const LinkedinButton = styled(BaseButton)`
   }
 `
 
+export const CreateButton = styled(Button)(({ theme }) => ({
+	width: '14em',
+	borderRadius: '10px',
+	color: theme.palette.getContrastText(purple[500]),
+	backgroundColor: purple[500],
+	'&:hover': {
+		backgroundColor: purple[700],
+	},
+	'&:focus': {
+		boxShadow: 'none',
+	},
+}));
