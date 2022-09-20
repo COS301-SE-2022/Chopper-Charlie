408fce9ec3cf11f1d013794c8ffdcbbfd1c2c680
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 12:57:26 +0200

    analytics-page

---
+++ frontend/routes/analytics/analytics.component.jsx
@@ -0,0 +1,31 @@
+import PipelineForm from '../../components/pipeline-form/pipeline-form.component';
+import PipelineItem from '../../components/pipline-item/pipeline-item.component';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectPipelines } from '../../store/pipelines/pipelines.selector';
+import Drawer from '../../components/drawer/drawer.component';
+import './analytics.styles.css';
+
+const Analytics = () => {
+	const pipelines = useSelector(selectPipelines);
+	return (
+		<div className='analytics-container'>
+			<h1>Your Pipelines</h1>
+			<br />
+			<PipelineForm />
+			<br />
+			<div>
+				{pipelines.map((pipelineItem) => {
+					return (
+						<PipelineItem
+							key={pipelineItem.title}
+							pipelineItem={pipelineItem}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Analytics;
