cd211248b5a2195d9da2ba421bf7e83149138a07
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 13:52:38 +0200

    firestore-get-user-accounts

--- frontend/src/utils/firebase/firebase.utils.js
+++ frontend/src/utils/firebase/firebase.utils.js
@@ -1,38 +1,40 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
-	getAuth,
-	signInWithPopup,
-	GoogleAuthProvider,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
-	onAuthStateChanged,
-	connectAuthEmulator
-} from 'firebase/auth';
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  connectAuthEmulator,
+} from "firebase/auth";
 import {
-	getFirestore,
-	doc,
-	getDoc,
-	setDoc,
-	collection,
-	writeBatch,
-	query,
-	getDocs,
-	onSnapshot,
-	connectFirestoreEmulator
-} from 'firebase/firestore';
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+  onSnapshot,
+  connectFirestoreEmulator,
+} from "firebase/firestore";
 import { getApp } from "firebase/app";
-import { getFunctions, connectFunctionsEmulator, httpsCallable } from "firebase/functions";
+import {
+  getFunctions,
+  connectFunctionsEmulator,
+  httpsCallable,
+} from "firebase/functions";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
-import { createAccountContainer } from '../azure/azure.utils';
+import { createAccountContainer } from "../azure/azure.utils";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -49,26 +51,30 @@ export const db = getFirestore();
 const storage = getStorage();
 // connectStorageEmulator(storage, "localhost", 9199);
 
-
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-	prompt: 'select_account',
+  prompt: "select_account",
 });
 
-
 export const signInWithGooglePopup = () =>
-	signInWithPopup(auth, googleProvider);
+  signInWithPopup(auth, googleProvider);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = doc(collectionRef, obj.title.toLowerCase());
+    batch.set(newDocRef, obj);
+  });
+  await batch.commit();
+};
 
-    export const addCollectionAndDocuments = async (
-        collectionKey,
-        objectsToAdd
-    ) => {
-        const collectionRef = collection(db, collectionKey);
-        const batch = writeBatch(db);
-        objectsToAdd.forEach((obj) => {
-            const newDocRef = doc(collectionRef, obj.title.toLowerCase());
-            batch.set(newDocRef, obj);
-        });
-        await batch.commit();
-    };
+export const getUsers = async () => {
+	const usersRef = collection(db, 'users');
+	const q = query(usersRef);
+	const querySnapshot = await getDocs(q);
+	return querySnapshot.docs.map((doc) => doc.data());
+}
