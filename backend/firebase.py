8201f139ce4f64b0b0fbfccf1bb908f3405be71b
Hamza <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 15:43:23 +0200

    firebase-admin-list-accounts

--- backend/firebase.py
+++ backend/firebase.py
@@ -24,3 +24,67 @@ db = firestore.client()
 # user3 = auth.get_user('cyC2i2MrJeQDE3UveMoaFGQ8lcH3')
 # print('isAdmin ' + str(user3.custom_claims.get('admin')) + ', isSuper ' + str(user3.custom_claims.get('super')))
 
+# List All the Users in the Database
+def list_accounts(uid):
+    try:
+        admin = auth.get_user(uid)
+        docs = db.collection("users").stream()
+        accounts = []
+        
+        # IF REQUESTING USER IS A SUPER USER
+        if admin.custom_claims.get('super') == True:
+            for doc in docs:
+                try:
+                    if auth.get_user(doc.id):
+                        if auth.get_user(doc.id).custom_claims == None or auth.get_user(doc.id).custom_claims.get('admin') == False:
+                            user = doc.to_dict()
+                            user['type'] = 'user'
+                            accounts.append(user)
+                            continue
+                        if auth.get_user(doc.id).custom_claims.get('super') == True:
+                            user = doc.to_dict()
+                            user['type'] = 'super'
+                            accounts.append(user)
+                            continue
+                        if auth.get_user(doc.id).custom_claims.get('admin') == True:
+                            user = doc.to_dict()
+                            user['type'] = 'admin'
+                            accounts.append(user)
+                            continue
+                except:
+                    print(doc.id + ' is not a user\n')
+
+            return {'accounts': accounts}
+        
+        # IF REQUESTING USER IS AN ADMIN USER
+        if admin.custom_claims.get('admin') == True:
+            for doc in docs:
+                try:
+                    if auth.get_user(doc.id):
+                        if auth.get_user(doc.id).custom_claims == None or auth.get_user(doc.id).custom_claims.get('admin') == False:
+                            user = doc.to_dict()
+                            user['type'] = 'user'
+                            accounts.append(user)
+                            continue
+                        if auth.get_user(doc.id).custom_claims.get('super') == True:
+                            user = doc.to_dict()
+                            user['type'] = 'super'
+                            user['containerName'] = None
+                            accounts.append(user)
+                            continue
+                        if auth.get_user(doc.id).custom_claims.get('admin') == True:
+                            user = doc.to_dict()
+                            user['type'] = 'admin'
+                            user['containerName'] = None
+                            accounts.append(user)
+                            continue
+                except:
+                    print(doc.id + ' is not a user\n')
+            return {'accounts': accounts}
+        
+        # IF REQUESTING USER IS A NORMAL USER
+        return {"ERROR": {"code": 403, "message": "FORBIDDEN - You do not have permission"}}
+    except:
+        
+        # IF REQUESTING USER IS NOT A USER
+        return {"ERROR": {"code": 401, "message": "UNAUTHORIZED"}}
