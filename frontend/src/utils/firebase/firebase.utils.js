0afa1d1a45a7836319146b92fce045d72daf50df
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 13:53:01 +0200

    firestore-get-pipelines

--- frontend/src/utils/firebase/firebase.utils.js
+++ frontend/src/utils/firebase/firebase.utils.js
@@ -78,3 +78,13 @@ export const getUsers = async () => {
 	const querySnapshot = await getDocs(q);
 	return querySnapshot.docs.map((doc) => doc.data());
 }
+
+export const getPipelines = async (user) => {
+	const docRef = doc(db, 'pipelines', user.uid);
+	const docSnap = await getDoc(docRef);
+	if (docSnap.exists()) {
+		return docSnap.data();
+	} else {
+		console.log('No such document!');
+	}
+};
