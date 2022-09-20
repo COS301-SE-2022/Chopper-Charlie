238cb8998bd67d18dee43012bcfe2534ca27912d
Hamza Allimia <93662311+hamza-torres@users.noreply.github.com>
2022-09-20 13:54:54 +0200

    firebase-sign-in-user

--- frontend/src/utils/firebase/firebase.utils.js
+++ frontend/src/utils/firebase/firebase.utils.js
@@ -167,3 +167,9 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 
 	return await createUserWithEmailAndPassword(auth, email, password);
 };
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	return await signInWithEmailAndPassword(auth, email, password);
+};
