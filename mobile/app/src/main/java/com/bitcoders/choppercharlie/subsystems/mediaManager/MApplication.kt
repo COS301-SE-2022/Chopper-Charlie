f505fe96b79ac02046f884cfbd2f2576e5bd54be
Yaasir Barmania <yaasirbarmania@gmail.com>
2022-09-27 17:33:30 +0200

    added files

---
+++ mobile/app/src/main/java/com/bitcoders/choppercharlie/subsystems/mediaManager/MApplication.kt
@@ -0,0 +1,13 @@
+package com.bitcoders.choppercharlie.subsystems.mediaManager
+
+import android.app.Application
+import android.content.Context
+import com.secneo.sdk.Helper
+
+class MApplication: Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        Helper.install(this)
+    }
+}
